396a383cf76028f0824e70a728aa402e77cf1a7c
s18840 <Mikoaj@DESKTOP-G6E9EB5>
2024-10-29 17:45:08 +0100

    Climate tab finished

--- apartments-fue/src/components/Climate.js
+++ apartments-fue/src/components/Climate.js
@@ -1,11 +1,162 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 import "../styles/styles.scss";
+import i18n from "i18next";
+import "../styles/Climate.scss";
+import WeatherChart from "./WeatherChart.js"
 function Climate() {
   const { t } = useTranslation();
 
   return (
-    <div>
-        Tu będą informacje o klimacie na wyspie
+    <div className="climate-container">
+      <div className="container">
+        <div className="climate-title">
+          <div>{t("Climate_title")}</div>
+        </div>
+        <div className="climate-descriptions-container">
+          <div>
+          <Trans i18nKey="Climate_description_1" 
+            components={{
+              }}/>
+          </div>
+          <div className="">
+          <Trans i18nKey="Climate_description_2" 
+            components={{
+              }}/>
+          </div>
+          <div>
+          <Trans i18nKey="Climate_description_3" 
+            components={{
+              }}/>
+          </div>
+        </div>
+        <div className="weather-chart">
+          <WeatherChart></WeatherChart>
+        </div>
+        <div className="four-seasons">
+          <div className="season-header">{t("Spring")}</div>
+          <div className="season-text">
+            <ul>
+              <li>
+              <Trans i18nKey="Spring_text_1" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Spring_text_2" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Spring_text_3" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Spring_text_4" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Spring_text_5" 
+            components={{
+              }}/>
+              </li>
+            </ul>
+          </div>
+          <div className="season-header">{t("Summer")}</div>
+          <div className="season-text">
+          <ul>
+              <li>
+              <Trans i18nKey="Summer_text_1" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Summer_text_2" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Summer_text_3" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Summer_text_4" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Summer_text_5" 
+            components={{
+              }}/>
+              </li>
+            </ul>
+          </div>
+
+          <div className="season-header">{t("Autumn")}</div>
+          <div className="season-text">
+          <ul>
+              <li>
+              <Trans i18nKey="Autumn_text_1" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Autumn_text_2" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Autumn_text_3" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Autumn_text_4" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Autumn_text_5" 
+            components={{
+              }}/>
+              </li>
+            </ul>
+          </div>
+
+          <div className="season-header">{t("Winter")}</div>
+          <div className="season-text">
+          <ul>
+              <li>
+              <Trans i18nKey="Winter_text_1" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Winter_text_2" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Winter_text_3" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Winter_text_4" 
+            components={{
+              }}/>
+              </li>
+              <li>
+              <Trans i18nKey="Winter_text_5" 
+            components={{
+              }}/>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ apartments-fue/src/components/WeatherChart.js
@@ -0,0 +1,95 @@
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import "../styles/WeatherChart.scss";
+ChartJS.register(CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend, ChartDataLabels);
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  datasets: [
+    {
+      label: 'Air Temperature (°C)',
+      data: [19, 19, 20, 21, 22, 23, 24, 25.5, 25, 24, 22, 20],
+      type: 'line',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgba(255, 99, 132, 0.2)',
+      fill: false,
+      datalabels: {
+        anchor: 'end',
+        align: 'top',
+        color: 'rgba(255, 99, 132, 1)',
+        font: {
+          weight: 'bold',
+        },
+      },
+    },
+    {
+      label: 'Rainfall (mm)',
+      data: [9, 7, 5, 2, 1, 0, 0, 0, 5, 6, 7, 11],
+      type: 'bar',
+      backgroundColor: 'rgba(54, 162, 235, 0.5)',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      datalabels: {
+        anchor: 'end',
+        align: 'top',
+        color: 'rgba(54, 162, 235, 1)',
+        font: {
+          weight: 'bold',
+        },
+      },
+    },
+    {
+      label: 'Water Temperature (°C)',
+      data: [16, 15.5, 16, 17, 18, 19, 20, 21, 22, 21, 19, 17],
+      type: 'line',
+      backgroundColor: 'rgb(75, 192, 192)',
+      borderColor: 'rgba(75, 192, 192, 0.2)',
+      fill: false,
+      datalabels: {
+        anchor: 'end',
+        align: 'top',
+        color: 'rgba(75, 192, 192, 1)',
+        font: {
+          weight: 'bold',
+        },
+      },
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    y: {
+      beginAtZero: true,
+      max: 30,
+      ticks: {
+        stepSize: 5,
+      },
+    },
+  },
+  plugins: {
+    legend: {
+      display: true,
+      position: 'top',
+    },
+    tooltip: {
+      enabled: true,
+    },
+    datalabels: {
+      display: true,
+      formatter: (value) => value,
+    },
+  },
+};
+
+function MyMixedChart() {
+  return (
+    <div className='weather-chart-container'>
+      <Line data={data} options={options} />
+    </div>
+  );
+}
+
+export default MyMixedChart;
